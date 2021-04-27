73f0439d2b1286fe4b80da3f0815d533ba2a5c91
Thanawat Petchsri <thanawat.petchsri@gmail.com>
2021-04-28 02:30:55 +0700

    Add some styles

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,6 +11,8 @@ const Banner = () => {
     <div className="banner">
       <div className="banner__heading">
         <h1>What's meant to be will always find a way</h1>
+        <p className="tagline">PawsUp help tou to find the best partners for your dearest fluffly friend</p>
+        <a href="#" className="btn btn--cta">Find a Match</a>
       </div>
     </div>
   )
